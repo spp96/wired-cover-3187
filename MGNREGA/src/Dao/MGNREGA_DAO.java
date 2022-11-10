418a550bce4ddb40d4b444713c7a4bb665bc901b
Shubham Patil <shubhamppatil1996@gmail.com>
2022-11-10 17:13:05 +0530

    day-3 work

--- MGNREGA/src/Dao/MGNREGA_DAO.java
+++ MGNREGA/src/Dao/MGNREGA_DAO.java
@@ -1,8 +1,32 @@
 package Dao;
 
 import java.sql.Connection;
+import java.util.List;
+
+import Exceptions.*;
+import Model.*;
 
 public interface MGNREGA_DAO {
+public BDO loginbdo(String usename,String password)throws BDOExceptions;
 	
-
+	public GPM loginGPM(String usename,String password)throws GPMExceptions;
+	
+	public String createproject(Projects project);
+	
+	public String createnewGPM(GPM gpm);
+	
+	public String allocateprojecttoGPM(int pid,int gpmid)throws ProjectsExceptions, GPMExceptions;
+	
+	public List<Projects> listofProject()throws ProjectsExceptions;
+	
+	public List<GPM> listofGPM()throws GPMExceptions;
+	
+	public String createemployee(Employee emp);
+	
+	public List<Employee> listofEmployee()throws EmployeeExceptions;
+	
+    public List<EmployeeList> employeeUnderProject()throws ProjectsExceptions;
+    
+    public String assignemptoproject(int pid,int empid)throws ProjectsExceptions,EmployeeExceptions;
+	
 }
